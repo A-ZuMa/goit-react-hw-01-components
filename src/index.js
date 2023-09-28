6cb2c0071046a9e22c4f18674899d5446c2b8ce8
ZuMa <azuley@gmail.com>
2023-09-28 19:16:35 +0300

    Test commit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
 
+import { username, tag, location, avatar, stats } from './user';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
